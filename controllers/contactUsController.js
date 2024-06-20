690a703bb4d74ba4e13702f4194b2f2c1a46ff0a
Ahmad Majed <ahmadmajed@Ahmads-MacBook-Air.local>
2024-06-20 16:52:17 +0300

    test

--- controllers/contactUsController.js
+++ controllers/contactUsController.js
@@ -19,6 +19,14 @@ const contactUs = asyncHandler(async (req, res) => {
       },
     });
 
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log("SMTP Configuration Error:", error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
+
     const mailOptions = {
       from: "info@midpointvape.co",
       to: "info@midpointvape.co",
